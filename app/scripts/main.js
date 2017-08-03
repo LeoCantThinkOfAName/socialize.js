b3426e99b31a308c450cb933680c9688ceb3e51d
leocantthinkofaname <i7851@hotmail.com>
2017-08-03 18:10:59 +0800

    some update and basic page layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,143 +1,28 @@
-(function($, window, document) {
+$(function() {
+    'use strict';
 
-    $.fn.Socialize = function(options) {
-        //api cdn's
-        const Api = {
-            facebook: ['//connect.facebook.net/en_US/sdk.js', 'https://www.facebook.com/sharer/sharer.php?u='],
-            twitter: ['https://twitter.com/intent/tweet?']
-        }
+    //when user click on tab
+    const changeTabs = () => {
+        const tabs = $('.content-tab'),
+              contents = $('.content-instruction'),
+              typeGraphs = $('.social-media-bg span'),
+              body = $('body'),
+              bg = $('body').data('bg'),
+              heading = $('header h1');
 
-        let tempObj = options ? options : {},
-            optionsObj = {
-                useApi: 'useApi' in tempObj ? tempObj.useApi : false,
-                appId: 'appId' in tempObj ? tempObj.appId : false,
-                version: 'version' in tempObj ? tempObj.version : false,
-                method: 'method' in tempObj ? tempObj.method : false,
-                href: 'href' in tempObj ? tempObj.href : false,
-                callback: 'callback' in tempObj ? tempObj.callback : false,
-                height: 'height' in tempObj ? tempObj.height : false,
-                width: 'width' in tempObj ? tempObj.width : false,
-                target: 'target' in tempObj ? tempObj.target : false,
-                hashtag: 'hashtag' in tempObj ? tempObj.hashtag : false,
-                type: 'type' in tempObj ? tempObj.type : false,
-                context: 'context' in tempObj ? tempObj.context : false,
-                user: 'user' in tempObj ? tempObj.user : false
-            }
+        for(let i = 0; i < tabs.length; i ++) {
+            $(tabs[i]).bind('click', function() {
+                $(tabs).removeClass('active');
+                $(contents).removeClass('active');
+                $(typeGraphs).removeClass('active');
 
-        let $this = this,
-            $thisData = $(this).data(),
-            $media;
-        
-        //extract data attribute name
-        function returnDataAttribute(ele) {
-            $.each(ele, function(index, value) {
-                $media = value;
+                $(this).addClass('active');
+                $(contents[i]).addClass('active');
+                $(typeGraphs[i]).addClass('active');
+                $(body).css({background: bg[i]});
+                $(heading).css({color: bg[i]});
             })
         }
-        returnDataAttribute($thisData);
-
-        //extract hashtags
-        function returnHashtags(arr) {
-            let string = '';
-
-            if(!arr) {
-                return;
-            }
-
-            for(let i = 0; i < arr.length; i ++) {
-                if(i < arr.length - 1) {
-                    string = string + arr[i] + ',';
-                } else {
-                    string = string + arr[i];
-                }
-            }
-            return '&hashtags=' + string;
-        }
-
-        switch($media) {
-            case 'facebook':
-                onFacebook();
-                console.log('You\'ve sucessfully inject socialize.js for facebook in your page!');
-                break;
-            case 'twitter':
-                onTwitter();
-                console.log('You\'ve sucessfully inject socialize.js for twitter in your page!');
-                break;
-        }
-
-        //disable anchor tag
-        function disableAnchor() {
-            for(let i = 0; i < $this.length; i ++) {
-                $($this[i]).prop('href', 'javascript:;');
-            }
-        }
-        
-        //facebook
-        function onFacebook() {
-            disableAnchor();
-
-            $this.bind('click', function() {
-
-                console.log('share via facebook');
-            })
-        }
-
-        function onTwitter() {
-            disableAnchor();
-
-            $this.bind('click', function() {
-
-                console.log('share via twitter');
-            })
-        }
-        // function mainFn() {
-        //     //extract arguments
-
-        //     if($media === 'facebook') {
-        //         if(useApi) {
-        //             //using facebook graph api
-        //             $.getScript(Api.facebook[0], function() {
-        //                 FB.init({
-        //                     appId: appId,
-        //                     version: version
-        //                 });
-
-        //                 $this.bind('click', function(e) {
-        //                     $(e.target).prop('href', 'javascript:;');
-
-        //                     FB.ui({
-        //                         method: method ? method : 'share',
-        //                         href: href ? href : encodeURI(window.location.href)
-        //                     }, callback);
-        //                 })
-        //             })
-        //         } else {
-        //             //default
-        //             $this.bind('click', function(e) {
-        //                 $(e.target).prop('href', 'javascript:;');
-        //                 href = href ? href : encodeURI(window.location.href);
-
-        //                 window.open(Api.facebook[1] + href, target, 'height=' + height + 'px, width=' + width + 'px');
-        //             })
-        //         }
-        //     } else if($media === 'twitter') {
-        //             $this.bind('click', function(e) {
-        //                 $(e.target).prop('href', 'javascript:;');
-        //                 href = href ? href : encodeURI(window.location.href);
-        //                 hashtag = returnHashtags(hashtag);
-
-        //                 if(hashtag) {
-        //                     let hay = Api.twitter + context + '&url=' + href + hashtag + '&via=' + user;
-        //                     console.log(hay)
-        //                 }
-        //             })
-        //     } else {
-                
-        //     }    
-        // }
-        // mainFn();
     }
-
-})(window.jQuery, window, document);
-$('[data-sm=facebook]').Socialize({useApi: true});
-$('[data-sm=twitter]').Socialize();
+    changeTabs();
+});
---
+++ app/scripts/socialize.js
@@ -0,0 +1,155 @@
+(function($, window, document) {
+
+    $.fn.Socialize = function(options) {
+        //api cdn's
+        const Api = {
+            facebook: ['//connect.facebook.net/en_US/sdk.js', 'https://www.facebook.com/sharer/sharer.php?u='],
+            twitter: ['https://twitter.com/intent/tweet?']
+        }
+
+        let tempObj = options ? options : {},
+            optionsObj = {
+                useApi: 'useApi' in tempObj ? tempObj.useApi : false,
+                appId: 'appId' in tempObj ? tempObj.appId : false,
+                version: 'version' in tempObj ? tempObj.version : false,
+                method: 'method' in tempObj ? tempObj.method : false,
+                href: 'href' in tempObj ? tempObj.href : false,
+                callback: 'callback' in tempObj ? tempObj.callback : false,
+                height: 'height' in tempObj ? tempObj.height : false,
+                width: 'width' in tempObj ? tempObj.width : false,
+                target: 'target' in tempObj ? tempObj.target : false,
+                hashtag: 'hashtag' in tempObj ? tempObj.hashtag : false,
+                type: 'type' in tempObj ? tempObj.type : false,
+                context: 'context' in tempObj ? tempObj.context : false,
+                user: 'user' in tempObj ? tempObj.user : false,
+                icon: 'icon' in tempObj ? tempObj.icon : false,
+                tagTitle: 'tagTitle' in tempObj ? tempObj.tagTitle : false
+            }
+
+        let $this = this,
+            $thisData = $(this).data(),
+            $media,
+            $url = encodeURI(window.location.href);
+        
+        //extract data attribute name
+        function returnDataAttribute(ele) {
+            $.each(ele, function(index, value) {
+                $media = value;
+            })
+        }
+        returnDataAttribute($thisData);
+
+        //generate icon
+        function iconGenerator() {
+            console.log('create icon...');
+        }
+
+        //extract hashtags
+        function returnHashtags(arr) {
+            let string = '';
+
+            if(!arr) {
+                return;
+            }
+
+            for(let i = 0; i < arr.length; i ++) {
+                if(i < arr.length - 1) {
+                    string = string + arr[i] + ',';
+                } else {
+                    string = string + arr[i];
+                }
+            }
+            return '&hashtags=' + string;
+        }
+
+        switch($media) {
+            case 'facebook':
+                onFacebook();
+                console.log('You\'ve sucessfully inject socialize.js for facebook in your page!');
+                break;
+            case 'twitter':
+                onTwitter();
+                console.log('You\'ve sucessfully inject socialize.js for twitter in your page!');
+                break;
+        }
+
+        //disable anchor tag
+        function disableAnchor() {
+            for(let i = 0; i < $this.length; i ++) {
+                $($this[i]).prop('href', 'javascript:;');
+            }
+        }
+        
+        //facebook
+        function onFacebook() {
+            disableAnchor();
+
+            $this.bind('click', function() {
+
+                console.log('share via facebook');
+
+                window.open(`${Api.facebook[1]}${$url}`, 'share', `width=${optionsObj.width ? optionsObj.width : 300},height=${optionsObj.height ? optionsObj.height : 300}`);
+            })
+        }
+
+        //twitter
+        function onTwitter() {
+            disableAnchor();
+
+            $this.bind('click', function() {
+
+                console.log('share via twitter');
+                window.open(`${Api.twitter[0]}${$url}`);
+            })
+        }
+
+        // function mainFn() {
+        //     //extract arguments
+        //     if($media === 'facebook') {
+        //         if(useApi) {
+        //             //using facebook graph api
+        //             $.getScript(Api.facebook[0], function() {
+        //                 FB.init({
+        //                     appId: appId,
+        //                     version: version
+        //                 });
+
+        //                 $this.bind('click', function(e) {
+        //                     $(e.target).prop('href', 'javascript:;');
+
+        //                     FB.ui({
+        //                         method: method ? method : 'share',
+        //                         href: href ? href : encodeURI(window.location.href)
+        //                     }, callback);
+        //                 })
+        //             })
+        //         } else {
+        //             //default
+        //             $this.bind('click', function(e) {
+        //                 $(e.target).prop('href', 'javascript:;');
+        //                 href = href ? href : encodeURI(window.location.href);
+
+        //                 window.open(Api.facebook[1] + href, target, 'height=' + height + 'px, width=' + width + 'px');
+        //             })
+        //         }
+        //     } else if($media === 'twitter') {
+        //             $this.bind('click', function(e) {
+        //                 $(e.target).prop('href', 'javascript:;');
+        //                 href = href ? href : encodeURI(window.location.href);
+        //                 hashtag = returnHashtags(hashtag);
+
+        //                 if(hashtag) {
+        //                     let hay = Api.twitter + context + '&url=' + href + hashtag + '&via=' + user;
+        //                     console.log(hay)
+        //                 }
+        //             })
+        //     } else {
+                
+        //     }    
+        // }
+        // mainFn();
+    }
+
+})(window.jQuery, window, document);
+// $('[data-sm=facebook]').Socialize({useApi: true});
+// $('[data-sm=twitter]').Socialize();
